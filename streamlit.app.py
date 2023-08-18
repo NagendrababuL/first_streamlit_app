9f5cfc4df32eea79e9a919206189689702ef2b05
NagendrababuL <142583270+NagendrababuL@users.noreply.github.com>
2023-08-18 15:40:27 +0530

    Update streamlit.app.py
    Changed the title ,added new smoothie & emojis added to all the items.

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My Partents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale , Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale , Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.text('🍌🥭 Sapota Milk Shake 🥝🍇')
