3b835970f57a93a6c39b2e86ddecfc8a5e044e3b
NagendrababuL <142583270+NagendrababuL@users.noreply.github.com>
2023-08-21 12:00:28 +0530

    Update streamlit.app.py
    typo

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,7 +2,7 @@ import streamlit
 #import pandas
 #import requests
 import snowflake.connector
-from urllib.error import error URLError
+from urllib.error import URLError
 
 streamlit.title('My Partents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
