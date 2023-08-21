909df5696a0687d04bf004c63aabf04ae812a62e
NagendrababuL <142583270+NagendrababuL@users.noreply.github.com>
2023-08-21 10:12:52 +0530

    Update streamlit.app.py
    own try

--- streamlit.app.py
+++ streamlit.app.py
@@ -36,4 +36,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
+streamlit.text("What fruit would you like to add:")
+add_my_fruit = my_cur.fetchone(jackfruit)
+streamlit.dataframe(add_my_fruit)
+streamlit.text("Thanks for adding "streamlit.dataframe(add_my_fruit))
