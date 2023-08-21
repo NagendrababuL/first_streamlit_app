84fec73a8703b28002643eb1be151ebca8bda9f7
NagendrababuL <142583270+NagendrababuL@users.noreply.github.com>
2023-08-21 10:49:00 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -22,17 +22,28 @@ Fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.i
 Fruits_to_show = my_fruit_list.loc[Fruits_selected]
 # Display the table on the page
 streamlit.dataframe(Fruits_to_show)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
 # New section to dispay fruityvice api response
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+    
+# New section to display fruityvice api responce
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
   else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 #streamlit.write('The user entered ', fruit_choice)
 
 except URLError as e
@@ -40,10 +51,4 @@ streamlit.error
 
 # streamlit.text(fruityvice_response.json())
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
